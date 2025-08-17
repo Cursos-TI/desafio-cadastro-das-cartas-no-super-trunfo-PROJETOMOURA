b820acc6fde88813eff78af9e422793b665b0784
Moura Gabriel <mouragabrielalves@gmail.com>
2025-08-17 21:58:41 +0000

    Meu_Super_Trunfo_iniciante

--- Meu_Super_Trunfo.c
+++ Meu_Super_Trunfo.c
@@ -2,14 +2,18 @@
 
 int main(){
 //variáveis
-    char codigo1[4], codigo2[4];
+    char codigo1[4], codigo2[4], estado1[2], estado2[2], cidade1[50], cidade2[50];
     int populacao1, populacao2, turistico1, turistico2;
     float area1, area2, pib1, pib2;
   
 //cadastro carta 1
     printf("\n=== Cadastro Carta 1 ===\n");
+    printf("Informe o Estado: ");
+    scanf("%s", estado1);
     printf("Informe o Código: ");
     scanf("%s", codigo1);
+    printf("Informe o Nome da Cidade: ");
+    scanf("%s", cidade1);
     printf("Informe a População: ");
     scanf("%d", &populacao1);
     printf("Informe a Área: ");
@@ -18,14 +22,16 @@ int main(){
     scanf("%f", &pib1);
     printf("Informe o Ponto Turístico: ");
     scanf("%d", &turistico1);
-    printf("\n");
-    printf("Cadastro feito com sucesso!\n");
-    printf("\n");
+    printf("\nCadastro feito com sucesso!\n");
 
 //Cadastro da Carta 2
     printf("\n=== Cadastro Carta 2 ===\n");
+    printf("Informe o Estado: ");
+    scanf("%s", estado2);
     printf("Informe o Código: ");
     scanf("%s", codigo2);
+    printf("Informe o Nome da Cidade: ");
+    scanf("%s", cidade2);
     printf("Informe a População: ");
     scanf("%d", &populacao2);
     printf("Informe a Área: ");
@@ -39,14 +45,18 @@ int main(){
 
 //Resultado
     printf("\n==== CARTA 1 ====\n");
+    printf("Estado: %s\n", estado1);
     printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos turísticos: %d\n", turistico1);
 
     printf("\n==== CARTA 2 ====\n");
+    printf("Estado: %s\n", estado2);
     printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
